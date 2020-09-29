55ada8135a93a6afb78ea0beb883042553da710a
康輔 <irohalove2525@gamil.com>
2020-09-29 15:25:56 +0000

    11.1.3終了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,23 +1,30 @@
 class AttendancesController < ApplicationController
+  before_action :set_user, only: :edit_one_month
+  before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :set_one_month, only: :edit_one_month
+  
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
- def update
-   @user = User.find(params[:user_id])
-   @attendance = Attendance.find(params[:id])
-   # 出勤時間が未登録であることを判定します。
-   if @attendance.started_at.nil?
-     if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
-       flash[:info] = "おはようございます！"
-     else
-      flash[:danger] = UPDATE_ERROR_MSG
+   def update
+     @user = User.find(params[:user_id])
+     @attendance = Attendance.find(params[:id])
+     # 出勤時間が未登録であることを判定します。
+     if @attendance.started_at.nil?
+       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
+         flash[:info] = "おはようございます！"
+       else
+        flash[:danger] = UPDATE_ERROR_MSG
+       end
+     elsif @attendance.finished_at.nil?
+       if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+         flash[:info] = "お疲れ様でした。"
+       else
+        flash[:danger] = UPDATE_ERROR_MSG
+       end
      end
-   elsif @attendance.finished_at.nil?
-     if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
-       flash[:info] = "お疲れ様でした。"
-     else
-      flash[:danger] = UPDATE_ERROR_MSG
-     end
+     redirect_to @user
    end
-   redirect_to @user
- end
+   
+   def edit_one_month
+   end
 end
